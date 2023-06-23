bf751083925484b381d275298f8c1a6627b9ac45
CZ041199 <136847406+CZ041199@users.noreply.github.com>
2023-06-23 15:55:49 -0300

    Add files via upload

---
+++ dicionario2.py
@@ -0,0 +1,20 @@
+lista = [1,2,3,3,5,7,9,9,11,31,8,8,47]
+conjunto_1 = set([1,2,3,3,5,7,9,9,11,31,8,8,47])
+
+
+
+print(conjunto_1)
+
+print(lista)
+
+conjunto_2 = {1,3,4,7,7,8,8,9,9,9,12,15,13,11,5,5}
+
+print(conjunto_2)
+
+conjunto_2.add(49)
+
+print(conjunto_2)
+
+conjunto_2.remove(3)
+
+print(conjunto_2)
